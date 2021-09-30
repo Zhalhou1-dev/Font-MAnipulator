f695ea6a4c0b4fd6f8a7c16cbb427ae579e934eb
Zhalhou1-dev <83404810+Zhalhou1-dev@users.noreply.github.com>
2021-09-30 12:31:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 
+rightwrist=0;
+leftwrist=0;
+difference=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
     canvas=createCanvas(550,500);
-    canvas.position(560,150);
+    canvas.position(590,150);
     posenet=ml5.poseNet(video,modelLoaded)
     posenet.on('pose',gotPoses)
+}
+function modelLoaded(){
+    console.log('Posenet is initialized!')
+}
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        leftwrist=results[0].pose.leftWrist.x;
+        rightwrist=results[0].pose.rightWrist.x;
+        difference=floor(leftwrist-rightwrist);
+
+    }
 }
 function draw(){
     background('#969A97');
     fill('#a1f542');
-}
-function modelLoaded(){
-    console.log('Posenet is initialized!')
+    stroke('#00001');
+    text("hello",50,400);
+    textSize(difference)
 }
